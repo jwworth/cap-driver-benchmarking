70690ffc930d5534913bba4e69b885f3f5a4c438
Jake Worth <jakeworth82@gmail.com>
2015-06-17 11:14:00 -0500

    First tests passing!

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'haml-rails'
+gem 'selenium-webdriver'
+gem 'capybara-webkit'
 
 gem 'jquery-rails'
 gem 'turbolinks'
---
+++ spec/features/benchmarking_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'benchmarking' do
+  def do_things
+    visit root_path
+    100.times do
+      find :css, 'h1'
+      assert_text 'Capybara Driver Benchmarking!'
+      click_button 'Here'
+    end
+  end
+
+  it 'does everything with rack_test' do
+    do_things
+  end
+
+  it 'does everything with selenium' do
+    Capybara.current_driver = :selenium
+    do_things
+    Capybara.use_default_driver
+  end
+
+  it 'does everthing with webkit' do
+    Capybara.current_driver = :webkit
+    do_things
+    Capybara.use_default_driver
+  end
+end
